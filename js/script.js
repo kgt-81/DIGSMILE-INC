60ecd58432845ee179f75ba65abb99d7c9bf1381
kgt-81 <97212951+kgt-81@users.noreply.github.com>
2022-02-16 14:27:23 +0900

    ヘッダー変更

--- js/script.js
+++ js/script.js
@@ -1,8 +1,3 @@
-
-
-
-
-
 $(function () {
 
   let hamburger_menu = document.getElementById('hamburger-menu');
@@ -20,8 +15,8 @@ for (let i = 0; i < menu_link.length; i++) {
     hamburger_menu.classList.remove('open');
     nav_wrapper.classList.remove('show');
     $('.nav-bg').stop(true,true).fadeOut();
-    document.removeEventListener('touchmove', disableScroll, { passive: false });
-    document.removeEventListener('mousewheel', disableScroll, { passive: false });
+    document.addEventListener('touchmove', disableScroll, { passive: false });
+    document.addEventListener('mousewheel', disableScroll, { passive: false });
     window.scrollTo({
       top: position,
       behavior: "smooth"
@@ -51,3 +46,29 @@ for (let i = 0; i < menu_link.length; i++) {
   });
   
 });
+
+window.addEventListener('scroll', header);
+window.addEventListener('load', header);
+function header(){
+  let heading_1 = document.getElementById('heading_1');
+  let heading_1_bottom = window.pageYOffset + heading_1.getBoundingClientRect().bottom;
+  let fv_img = document.getElementById('fv-img');
+  let fv_img_height = fv_img.clientHeight;
+  let fv = document.getElementById('fv');
+  let fv_height = fv.clientHeight;
+  let hamburger_menu = document.getElementById('hamburger-menu');
+  let hamburger_menu_bottom = window.pageYOffset + hamburger_menu.getBoundingClientRect().bottom;
+  if ( fv_img_height < heading_1_bottom ){
+    heading_1.classList.add("black");
+  }else {
+    heading_1.classList.remove("black");
+    // もう一度ウィンドウが要素の上に来た時
+  };
+
+  if ( fv_height < hamburger_menu_bottom ){
+    hamburger_menu.classList.add("black");
+  }else {
+    hamburger_menu.classList.remove("black");
+    // もう一度ウィンドウが要素の上に来た時
+  };
+};
